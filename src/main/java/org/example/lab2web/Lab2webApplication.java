1609d0dfcfb432cf8fd76a1f8950fec1b70f1dab
ㅤАндрій <andriy.antonov7@gmail.com>
2024-10-10 20:44:39 +0300

    first commit

---
+++ src/main/java/org/example/lab2web/Lab2webApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab2web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2webApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2webApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/lab2web/Product.java
@@ -0,0 +1,11 @@
+package org.example.lab2web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+}
